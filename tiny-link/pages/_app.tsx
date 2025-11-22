d32b0b2b51b769dd1b878238899f468dc8d8730d
Aadarsh <aadarshchourasiya225@gmail.com>
2025-11-23 01:56:14 +0530

    Add favicon and update _app.tsx for improved SEO

--- tiny-link/pages/_app.tsx
+++ tiny-link/pages/_app.tsx
@@ -1,6 +1,14 @@
-import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <title>TinyLink</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
